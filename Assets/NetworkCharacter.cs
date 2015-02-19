db94f011bd12cd48b97b58ccb1d9260320e75e82
mafer09 <m.briceno09@gmail.com>
2015-02-19 12:51:28 -0600

    Added Sound to objects

--- Assets/NetworkCharacter.cs
+++ Assets/NetworkCharacter.cs
@@ -5,26 +5,37 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 
     Vector3 realPosition = Vector3.zero;
     Quaternion realRotation = Quaternion.identity;
-    //public GUIText winText;
-    private int realCount;
+    private int myCount = 0;
+    private int combineCount = 0;
+    public AudioClip BiteSound;
+    private GUIText countText;
+
+    void UpdateCount()
+    {
+        //countText.text = string.Format("My Count: {0}\nPlayer 2: {1}", myCount, combineCount);
+        countText.text = string.Format("My Count: {0}", myCount);
+    }
 
 	void Start () {
+        countText = GameObject.Find("Count Text").GetComponent<GUIText>();
 
-        realCount = 0;
-        PlayerPrefs.SetInt("OtherPlayerScore", realCount);
 	}
 	
 	void Update () {
         if (photonView.isMine)
         {
-            
+            if (myCount >= 6)
+            {
+                //
+            }
         }
         else
         {
             transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
             transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
-            PlayerPrefs.SetInt("OtherPlayerScore", realCount);
         }
+
+        UpdateCount();
 	}
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -33,13 +44,24 @@ public class NetworkCharacter : Photon.MonoBehaviour {
         {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
-            stream.SendNext(PlayerPrefs.GetInt("OtherPlayerCount"));
+            stream.SendNext(myCount);
         }
         else
         {
             realPosition = (Vector3) stream.ReceiveNext();
             realRotation = (Quaternion)stream.ReceiveNext();
-            realCount = System.Convert.ToInt32(stream.ReceiveNext());
+            combineCount = System.Convert.ToInt32(stream.ReceiveNext());
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            
+            other.gameObject.SetActive(false);
+            audio.Play();
+            myCount++;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,10 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
     public float speed;
-    public GUIText countText;
-    //public GUIText winText;
-    private int count;
 
     void Start()
     {
-       count = 0;
-       //winText.text = "";
-       countText = GameObject.Find("Count Text").GetComponent<GUIText>();
-       SetCountText();
+        
     }
     void FixedUpdate() //Before performing any physics calculations
     {
@@ -23,24 +17,5 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rigidbody.AddForce(movement * speed * Time.deltaTime);      //Time.deltaTime makes the input smooth and frame independent
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "PickUp")
-        {
-            other.gameObject.SetActive(false);
-            count++;
-            SetCountText();
-        }
-    }
-    void SetCountText()
-    {
-       countText.text = string.Format("My Score: {0}", count);
-
-      /* if (count >= 13 || count == 6)
-       {
-           winText.text = string.Format("You Won!");
-       }*/
     }
 }
